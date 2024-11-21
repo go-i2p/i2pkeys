ce5a2a34aa06fdeea7dcfcbcf6b246833b4fa5a2
eyedeekay <hankhill19580@gmail.com>
2024-11-21 18:51:01 -0500

    use external logger

--- log.go
+++ log.go
@@ -1,48 +1,21 @@
 package i2pkeys
 
 import (
-	"github.com/sirupsen/logrus"
-	"io/ioutil"
-	"os"
-	"strings"
-	"sync"
+	"github.com/go-i2p/logger"
 )
 
 var (
-	log  *logrus.Logger
-	once sync.Once
+	log *logger.Logger
 )
 
 func InitializeI2PKeysLogger() {
-	once.Do(func() {
-		log = logrus.New()
-		// We do not want to log by default
-		log.SetOutput(ioutil.Discard)
-		log.SetLevel(logrus.PanicLevel)
-		// Check if DEBUG_I2P is set
-		if logLevel := os.Getenv("DEBUG_I2P"); logLevel != "" {
-			log.SetOutput(os.Stdout)
-			switch strings.ToLower(logLevel) {
-			case "debug":
-				log.SetLevel(logrus.DebugLevel)
-			case "warn":
-				log.SetLevel(logrus.WarnLevel)
-			case "error":
-				log.SetLevel(logrus.ErrorLevel)
-			default:
-				log.SetLevel(logrus.DebugLevel)
-			}
-			log.WithField("level", log.GetLevel()).Debug("Logging enabled.")
-		}
-	})
+	logger.InitializeGoI2PLogger()
+	log = logger.GetGoI2PLogger()
 }
 
 // GetI2PKeysLogger returns the initialized logger
-func GetI2PKeysLogger() *logrus.Logger {
-	if log == nil {
-		InitializeI2PKeysLogger()
-	}
-	return log
+func GetI2PKeysLogger() *logger.Logger {
+	return logger.GetGoI2PLogger()
 }
 
 func init() {
